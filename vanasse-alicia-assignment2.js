7335ff1738aebed103393fe073dd34fa8fb63739
ADVanasse <advanasse@fullsail.edu>
2015-01-15 02:41:27 -0500

    Prompt added.

--- vanasse-alicia-assignment2.js
+++ vanasse-alicia-assignment2.js
@@ -5,18 +5,35 @@
 // Project 2
 
 
-// My Variables
-var bigTrip = ("Are you up for a big trip?");
-var whereTo = ["Lima", "San Diego", "Sydney", "Miami"];
-var flightTime = [8, 6, 24, 2];
-areYouComing = true;
+// Global Variables
+var myCamel = "Samuel";
+var camelWife = "Pam";
+var timeLost = 7;
+var missHer = true;
+
 
 
 //My main code
 
+console.log(myCamel + " and I are still desperate to find " + camelWife + ".");
+console.log("She's been gone for far too long now. A whole " + timeLost + "days."); 
+
+	//Prompt for Procedure
+	
+var youreSearchingRight = prompt("Have you continued to look for " + camelWife + "?!", "Of course");
+
+if(youreSearchingRight === "Of course") { 
+	console.log("Oh, that's great! Hopefully you remember where!");
+} else {
+	console.log("But you promised you'd help! She's been gone for so long. I hope she's okay.");
+};
 
 
-//Procedure Flowchart
+
+//Procedure Function
+
+
+
 
 //Boolean Function
 
