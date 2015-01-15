ba5bb25c7d0161839be117602b9019a078dd2b82
ADVanasse <advanasse@fullsail.edu>
2015-01-15 00:40:42 -0500

    corrected file name.

---
+++ vanasse-alicia-assignment2.js
@@ -0,0 +1,27 @@
+
+// Alicia Vanasse
+// SDI 1501
+// January 15, 2015
+// Project 2
+
+
+// My Variables
+var bigTrip = ("Are you up for a big trip?");
+var whereTo = ["Lima", "San Diego", "Sydney", "Miami"];
+var flightTime = [8, 6, 24, 2];
+areYouComing = true;
+
+
+//My main code
+
+
+
+//Procedure Flowchart
+
+//Boolean Function
+
+//Number Function
+
+//String Function
+
+
